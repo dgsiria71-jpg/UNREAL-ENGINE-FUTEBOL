b5c12dba56ab0c50024bc88a6f8af65174c1d4a6
dgsiria71-jpg <dgsiria71@gmail.com>
2026-09-13 13:50:43 -0300

    Pin GitHub uploader to canonical account

--- tools/PUBLICAR_MASTER_NO_GITHUB.bat
+++ tools/PUBLICAR_MASTER_NO_GITHUB.bat
@@ -1,5 +1,9 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+set "GIT_CONFIG_COUNT=1"
+set "GIT_CONFIG_KEY_0=credential.username"
+set "GIT_CONFIG_VALUE_0=dgsiria71-jpg"
+echo GitHub account requested: dgsiria71-jpg
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0PUBLICAR_MASTER_NO_GITHUB.ps1"
 exit /b %errorlevel%
